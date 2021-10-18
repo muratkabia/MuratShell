40dbbbc249ca4f85ff8def725ad75bfbdb2e7736
Murat Kaan Tekeli <77919456+muratkabia@users.noreply.github.com>
2021-10-18 15:53:55 +0300

    Create Help.cs

---
+++ MuratShell/Komutlar/Help.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuratShell.Komutlar
+{
+    class Help
+    {
+
+        public Help()
+        {
+            Console.WriteLine("exit, Exit MuratShell");
+            Console.WriteLine("winfetch, neofetch but for windows and in MuratShell!");
+            Console.WriteLine("clear, Clear the console");
+            Console.WriteLine("tetris, ...");
+            Console.WriteLine("helloworld, You are starting a beatiful and painfull journey");
+            Console.WriteLine("rect, calculate the area of a rectangle");
+        }
+
+    }
+}
